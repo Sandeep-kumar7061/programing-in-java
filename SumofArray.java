9e1c5257bb0fc035e0e1740e83ab05a92cb74d5d
sandeep-kumar7061 <sandeep.kumar.patel7061@gmail.com>
2025-06-02 20:43:10 +0530

    input

---
+++ SumofArray.java
@@ -0,0 +1,23 @@
+ // sum of all elements in an array
+
+ import java.util.Scanner;
+ public class SumofArray {
+    public static void main(String[]args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int [n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        
+        }
+        System.out.println("The sum of the array elements is: " +sum);
+        sc.close();
+    }
+
+ }
