f5783b94caebab94aef523c4c4dece84f139a67e
sandeep-kumar7061 <sandeep.kumar.patel7061@gmai.com>
2025-06-04 20:38:23 +0530

    Add PrintName class and refactor month class to use switch expression

--- month.java
+++ month.java
@@ -4,51 +4,26 @@ public class month{
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter the month number (1-12): ");
             int month = sc.nextInt();
-            String monthName = "";
-
-            switch (month) {
-                case 1:
-                    monthName = "January";
-                    break;
-                case 2:
-                    monthName = "February";
-                    break;
-                case 3:
-                    monthName = "March";
-                    break;
-                case 4:
-                    monthName = "April";
-                    break;
-                case 5:
-                    monthName = "May";
-                    break;
-                case 6:
-                    monthName = "June";
-                    break;
-                case 7:
-                    monthName = "July";
-                    break;
-                case 8:
-                    monthName = "August";
-                    break;
-                case 9:
-                    monthName = "September";
-                    break;
-                case 10:
-                    monthName = "October";
-                    break;
-                case 11:
-                    monthName = "November";
-                    break;
-                case 12:
-                    monthName = "December";
-                    break;
-                default:
-                    System.out.println("Invalid month number. Please enter a number between 1 and 12.");
-            }
+            String monthName = switch (month) {
+                case 1 -> "January";
+                case 2 -> "February";
+                case 3 -> "March";
+                case 4 -> "April";
+                case 5 -> "May";
+                case 6 -> "June";
+                case 7 -> "July";
+                case 8 -> "August";
+                case 9 -> "September";
+                case 10 -> "October";
+                case 11 -> "November";
+                case 12 -> "December";
+                default -> "";
+            };
 
             if (!monthName.isEmpty()) {
                 System.out.println("The name of the month is: " + monthName);
+            } else {
+                System.out.println("Invalid month number. Please enter a number between 1 and 12.");
             }
         }
     }
